b41b743ec69a70f81a68e6f2cabf941050cad767
David <dguerrerom01@gmail.com>
2019-03-22 12:06:15 +0100

    Final

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { File } from '@ionic-native/file'
 
 @NgModule({
   declarations: [
@@ -22,6 +23,7 @@ import { HomePage } from '../pages/home/home';
     HomePage
   ],
   providers: [
+    File,
     StatusBar,
     SplashScreen,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, Platform } from 'ionic-angular';
+import { File } from '@ionic-native/file';
 
 @Component({
   selector: 'page-home',
@@ -7,7 +8,45 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+
+  url: any;
+  directorios: any;
+
+  constructor(public navCtrl: NavController, private file: File, private platform: Platform) {
+    
+    this.platform.ready().then(() => {
+      
+      
+      this.file.listDir(this.file.externalRootDirectory, '').then(
+        (list) => {
+          console.log(list);
+          this.directorios = list;
+        }
+      ).catch(e => console.log(e));
+
+
+    }).catch(e => console.log(e));
+
+  }
+
+  abrir(directorio){
+    console.log(directorio.fullPath)
+    this.file.listDir(this.file.externalRootDirectory, directorio.fullPath.substring(1, directorio.fullPath.length)).then(
+      (list) => {
+        console.log(list);
+        this.directorios = list;
+      }
+    ).catch(e => console.log(e));
+  }
+
+  retroceder(){
+    console.log(this.file.externalRootDirectory)
+    this.file.listDir(this.file.externalRootDirectory, '').then(
+      (list) => {
+        console.log(list);
+        this.directorios = list;
+      }
+    ).catch(e => console.log(e));
 
   }
 
